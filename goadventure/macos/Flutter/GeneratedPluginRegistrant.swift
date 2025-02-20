ef20e245d76ffafe751771c72778e32cedfffdb1
Paweł Charysz <pawel.erdahil@gmail.com>
2025-02-20 18:06:21 +0100

    add basic osm map widget

--- goadventure/macos/Flutter/GeneratedPluginRegistrant.swift
+++ goadventure/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+<<<<<<< HEAD
 import flutter_secure_storage_macos
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+=======
+import geolocator_apple
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+>>>>>>> ea6e10b (add basic osm map widget)
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
