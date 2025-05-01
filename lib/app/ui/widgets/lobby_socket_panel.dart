3215a867fb7660029f2b947e58faefcf9e52f0b7
Paweł Charysz <pawel.erdahil@gmail.com>
2025-05-01 04:27:05 +0200

    deps & test lobby with socket

---
+++ lib/app/ui/widgets/lobby_socket_panel.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:gotale/app/services/websocket_service.dart';
+
+// only for testing sockets for now
+
+
+class LobbySocketPanel extends StatefulWidget {
+  final String jwtToken;
+  final String lobbyId;
+
+  const LobbySocketPanel({
+    Key? key,
+    required this.jwtToken,
+    required this.lobbyId,
+  }) : super(key: key);
+
+  @override
+  State<LobbySocketPanel> createState() => _LobbySocketPanelState();
+}
+
+class _LobbySocketPanelState extends State<LobbySocketPanel> {
+  final SocketService socketService = Get.find();
+  final List<String> _logs = [];
+
+  void _log(String msg) {
+    setState(() => _logs.add(msg));
+  }
+
+  void _error(String err) {
+    _log("Błąd: $err");
+    Get.snackbar("Błąd", err, snackPosition: SnackPosition.BOTTOM);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    socketService.connectToLobby(
+      jwtToken: widget.jwtToken,
+      lobbyId: widget.lobbyId,
+      onLog: _log,
+      onError: _error,
+    );
+  }
+
+  @override
+  void dispose() {
+    socketService.disconnect(() => _log("Rozłączono"));
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            socketService.sendLobbyMessage(widget.lobbyId, _log);
+          },
+          child: Text("Wyślij wiadomość"),
+        ),
+        const SizedBox(height: 12),
+        const Text("Logi połączenia:"),
+        Container(
+          height: 150,
+          width: double.infinity,
+          padding: const EdgeInsets.all(8),
+          color: Colors.black12,
+          child: SingleChildScrollView(
+            child: Text(_logs.join('\n')),
+          ),
+        ),
+      ],
+    );
+  }
+}
